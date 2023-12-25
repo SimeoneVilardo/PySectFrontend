e030a9c57aefcb6a81eddbf3b65c0ddd7e22745a
SimeoneVilardo <simeone.vilardo@gmail.com>
2023-12-25 06:53:17 +0100

    Add suspanse in routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,18 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import ChallengeDetails from './pages/ChallengeDetails'
-import NotFound from './pages/NotFound'
+import { Route, Routes } from 'react-router-dom'
+import { lazy, Suspense } from 'react';
+const Home = lazy(() => import('./pages/Home'))
+const ChallengeDetails = lazy(() => import('./pages/ChallengeDetails'))
+const NotFound = lazy(() => import('./pages/NotFound'))
 
 function App() {
   return (
-    <BrowserRouter>
+    <Suspense fallback={<div className="container">Loading...</div>}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/challenge/:challengeId" element={<ChallengeDetails />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </Suspense>
   )
 }
 
--- client/src/pages/ChallengeDetails.tsx
+++ client/src/pages/ChallengeDetails.tsx
@@ -4,7 +4,7 @@ import Challenge from '../models/Challenge';
 import '../styles/challengeSubmission.css'
 
 const ChallengeDetails = () => {
-    var { challengeId } = useParams();
+    let { challengeId } = useParams();
 
     const [challenge, setChallenge] = useState<Challenge | null>(null);
 
@@ -14,14 +14,13 @@ const ChallengeDetails = () => {
                 const response = await fetch(`/api/challenge/${challengeId}`);
                 const challengeJson = await response.json();
                 setChallenge(challengeJson);
+                console.log(challengeJson);
             } catch (error) {
                 console.error('Error fetching item:', error);
             }
         };
         fetchChallenge();
-    }, []);
-
-    console.log(challenge)
+    }, [challengeId]);
 
     return (
         <ol style={{ "--length": "5" } as any} role="list">
@@ -30,16 +29,6 @@ const ChallengeDetails = () => {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing diam donec adipiscing tristique risus.</p>
             </li>
         </ol>
-    )
-
-    return (
-        <>
-            <h1>Name: {challenge?.name}</h1>
-            <h1>Description: {challenge?.description}</h1>
-            <h1>Points: {challenge?.points}</h1>
-            <h1>Input Sample: {challenge?.input_sample}</h1>
-            <h1>Output Sample: {challenge?.output_sample}</h1>
-        </>
     )
 }
 
