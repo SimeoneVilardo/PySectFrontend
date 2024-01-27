72b245750ffa9ef9445b1f670bee2203cce561c6
SimeoneVilardo <simeone.vilardo@gmail.com>
2024-01-27 04:47:44 +0100

    Fix typo in for lopp

--- client/src/components/ChallengeDetailCard.tsx
+++ client/src/components/ChallengeDetailCard.tsx
@@ -45,7 +45,7 @@ const ChallengeDetailCard = ({ challenge }: ChallengeDetailCardProps) => {
 
   const renderPoints = () => {
     const stars = [];
-    for (let i = 1; i <= 10; i = i++) {
+    for (let i = 1; i <= 10; i++) {
       stars.push(<input key={i} type="radio" name="points" className={`cursor-default animate-none bg-primary mask mask-star-2 mask-half-${i % 2 == 1 ? 1 : 2}`} checked={challenge.points*2 == i} readOnly disabled />);
     }
     return <div className="card-actions justify-start mt-2 gap-1">
