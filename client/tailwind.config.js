bf8b7514eb348b1b26c8382a1aee98b2218db006
Simeone Vilardo <simeone.vilardo@vedrai.com>
2024-01-28 00:37:29 +0100

    More pink

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  plugins: [require("@tailwindcss/typography"), require('daisyui')],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["business", "corporate", "valentine"]
+    themes: [
+      "business",
+      "corporate",
+      {
+        valentine: {
+          ...require("daisyui/src/theming/themes")["valentine"],
+          primary: "#f472b6",
+          secondary: "#c084fc",
+          accent: "#93c5fd",
+          accent: "#93c5fd",
+        },
+      },
+    ],
   },
-}
-
+};
