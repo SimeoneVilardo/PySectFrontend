876771d781c76a3c3f65ac1bf14de2b01a926227
Simeone Vilardo <simeone.vilardo@vedrai.com>
2024-01-28 01:03:38 +0100

    Use primary color for challenge card

--- client/src/components/ChallengeHomeCard.tsx
+++ client/src/components/ChallengeHomeCard.tsx
@@ -1,27 +1,29 @@
 import { Link } from "react-router-dom";
-import Challenge from "../models/Challenge"
+import Challenge from "../models/Challenge";
 
 interface ChallengeHomeCardProps {
-    challenge: Challenge;
+  challenge: Challenge;
 }
-
 
 const ChallengeHomeCard = ({ challenge }: ChallengeHomeCardProps) => {
-
-    return (
-        <div className="indicator">
-            <span className="indicator-item badge badge-primary">{challenge.points} ⭐</span>
-            <div className="card w-96 bg-secondary shadow-xl">
-                <div className="card-body">
-                    <h2 className="card-title">{challenge.name}</h2>
-                    <p>{challenge.subtitle}</p>
-                    <div className="card-actions justify-end">
-                        <Link className="btn" to={`/challenge/${challenge.id}`}>See More</Link>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="indicator">
+      <span className="indicator-item badge badge-secondary">
+        {challenge.points} ⭐
+      </span>
+      <div className="card w-96 bg-primary shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">{challenge.name}</h2>
+          <p>{challenge.subtitle}</p>
+          <div className="card-actions justify-end">
+            <Link className="btn" to={`/challenge/${challenge.id}`}>
+              See More
+            </Link>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ChallengeHomeCard
+export default ChallengeHomeCard;
